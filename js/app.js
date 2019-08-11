b113edbf32adaab737067cc9b7bd723b2d07f5be
Sergey Drozdov <sergeydrozdov2003@mail.ru>
2019-08-12 00:37:56 +0500

    Four commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$('.button_menu').click(function() {
+	$('.name_button').hide();
+	$(this).css({'text-align' : 'left'});
+	$(this).css({'margin-left' : '23px'});
+	$('.leftMenu').css({'width' : '80px'}).css({'transition' : '0.3s'});
+	function hide_close() {
+		$('.button_menu').hide();
+		$('.button_menu_open').show();
+	}
+	setTimeout(hide_close, 0);
+});
+
+
+$('.button_menu_open').click(function() {
+	$('.leftMenu').css({'width' : '300px'}).css({'transition' : '0.3s'});
+	$('.button_menu').css({'text-align' : 'center'});
+		$('.button_menu').show();
+		$('.button_menu_open').hide();
+	function hide_close() {
+		$('.name_button').show();
+	}
+	setTimeout(hide_close, 200);
+});
+
